012042fc0dea493336f6946d2a62b78fb309c309
Tdeptra98 <98nguyenngoctuyen@gmail.com>
2020-10-12 15:55:05 +0700

    day 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import Pagination from './components/Pagination/Pagination';
+import PostList from './components/Pagination/';
 function App() {
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 11,
+  });
+  function handlePageChange(newPage) {
+    console.log('new page: ', newPage);
+    setFilter({
+      ...filter,
+      _page: newPage,
+    });
+  }
+  const [filter, setFilter] = useState({
+    _page: 1,
+    _limit: 10,
+  });
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const requestURL = `http://js-post-api.herokuapp.com/api/posts?_limit=${filter._limit}&_page=${filter._page}`;
+        const response = await fetch(requestURL);
+        const responseJSON = await response.json();
+        console.log({ responseJSON });
+        const { pagination } = responseJSON;
+        setPagination(pagination);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData();
+  }, [filter]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      ></Pagination>
     </div>
   );
 }
---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+PostList.propTypes = {};
+
+function PostList(props) {
+  return <div></div>;
+}
+
+export default PostList;
